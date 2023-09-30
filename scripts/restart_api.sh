8e08f49c921bdaf27af51700a643dd74991923dd
nikhil_777 <tandlenikhilraj@gmail.com>
2023-09-30 10:48:02 +0530

    venv path updates

--- scripts/restart_api.sh
+++ scripts/restart_api.sh
@@ -5,7 +5,7 @@
 fuser -n tcp -k 443
 
 #start the gpt api
-cd /home/nikhil/Repositories/ScrapeN && source virtualenv/bin/activate
+cd /home/nikhil/Repositories/ScrapeN && source .venv/bin/activate
 
 #start the gpt api
 gunicorn --workers=3 --threads=3 --reload --bind 0.0.0.0:443 --timeout 2500 --keyfile=/home/nikhil/privkey.pem --certfile=/home/nikhil/fullchain.pem --reload --capture-output --error-logfile logs/error_log.txt --access-logfile logs/guicorn_log.txt -k uvicorn.workers.UvicornWorker app:app &
--- scripts/start_api.sh
+++ scripts/start_api.sh
@@ -2,7 +2,7 @@
 ## Change the path of the virtualenv and the gunicorn command accordingly
 
 #start the gpt api
-cd /home/nikhil/Repositories/ScrapeN && source virtualenv/bin/activate
+cd /home/nikhil/Repositories/ScrapeN && source .venv/bin/activate
 
 #start the gpt api
 gunicorn --workers=3 --threads=3 --reload --bind 0.0.0.0:443 --timeout 2500 --keyfile=/home/nikhil/privkey.pem --certfile=/home/nikhil/fullchain.pem --reload --capture-output --error-logfile logs/error_log.txt --access-logfile logs/guicorn_log.txt -k uvicorn.workers.UvicornWorker app:app &
